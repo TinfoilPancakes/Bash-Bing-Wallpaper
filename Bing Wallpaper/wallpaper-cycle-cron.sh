b0746de73318d8442055ff1b33fa8ef910a0c478
Prateek R Patil <prateek.patil@gmail.com>
2016-08-06 14:35:11 -0700

    Initial commit - will update.

---
+++ Bing Wallpaper/wallpaper-cycle-cron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+USER=prp
+PID=$(pgrep -u $USER gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+GSETTINGS_BACKEND=dconf
+
+PICTURE_DIR="/home/prp/Pictures/Background Slideshow/"
+
+NEW_BACKGROUND=$(shuf -n1 -e "$PICTURE_DIR"*.jpg)
+
+gsettings set org.gnome.desktop.background picture-uri "file://$NEW_BACKGROUND"
+
+NEW_BACKGROUND=$(shuf -n1 -e "$PICTURE_DIR"*.jpg)
+
+gsettings set org.gnome.desktop.screensaver picture-uri "file://$NEW_BACKGROUND"
